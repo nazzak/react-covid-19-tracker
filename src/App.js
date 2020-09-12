dae1bd5d9ab2da71ffe5176ac3f28ae9920551ee
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-12 12:48:26 +0200

    added Map component and some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { MenuItem, FormControl, Select } from '@material-ui/core'
+import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core'
 import './App.css';
 import InfoBox from './components/InfoBox';
+import Map from "./components/Map"
 
 function App() {
 
@@ -65,50 +66,42 @@ function App() {
 
   return (
     <div className="app">
+      <div className='app__left'>
+        <div className="app__header">
+          <h1>COVID 19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+        </div>
 
-      <div className="app__header">
-        <h1>COVID 19 TRACKER</h1>
-        <FormControl className="app_dropdown">
-          <Select
-            variant="outlined"
-            value={country}
-            onChange={onCountryChange}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {
-              countries.map(country => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-          </Select>
-        </FormControl>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus cases" value={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" value={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" value={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+        <Map />
+
       </div>
 
-      <div className="app__stats">
-        <InfoBox title="Coronavirus cases" value={countryInfo.todayCases} total={countryInfo.cases} />
-        <InfoBox title="Recovered" value={countryInfo.todayRecovered} total={countryInfo.recovered} />
-        <InfoBox title="Deaths" value={countryInfo.todayDeaths} total={countryInfo.deaths} />
-      </div>
-
-
-
-
-
-      {/* Header */}
-
-
-      {/*  */}
-
-      {/* InfoBox*/}
-      {/* InfoBox */}
-      {/* InfoBox */}
-
-      {/* Table */}
+      <Card className="app_right">
+        {/* Table */}
       {/* Graph */}
+      <CardContent>
+        <h3>Live cases by country</h3>
+        <h3>Worldwide new cases</h3>
+      </CardContent>
+      </Card>
 
-      {/* Map : stopped at 1.45
-      https://www.youtube.com/watch?v=cF3pIMJUZxM&t=1291s
-      */}
-      
     </div>
   );
 }
