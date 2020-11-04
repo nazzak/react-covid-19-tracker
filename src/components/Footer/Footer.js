425f058afb629b7ea1d7fa942c932510f899e438
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-11-04 13:40:53 +0100

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <h3>This website was designed by Nazzak</h3>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <h3>This website was designed by Nazim SAIBI</h3>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
